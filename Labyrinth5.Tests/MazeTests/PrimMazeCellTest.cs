a78a24d8a6c2f70b224cc7cc834f4c2a8d630dd1
martin-dzhonov <martin.dzhonov@gmail.com>
2014-07-21 13:05:21 +0300

    Added maze cell tests

---
+++ Labyrinth5.Tests/MazeTests/PrimMazeCellTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Labyrinth5.Common.MazeComponents;
+using Labyrinth5.Common.MazeComponents.Cells;
+using Labyrinth5.Common;
+namespace Labyrinth5.Tests.MazeTests
+{
+    [TestClass]
+    public class PrimMazeCellTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void MatrixCoordinatesThrowExcepion()
+        {
+            PrimCell cell = new PrimCell(0, 0);
+            cell.Position = new MatrixCoordinates(-10, -10);
+        }
+    }
+}
