5fdb8f91f48e7f4fc727edbfea083d65fcce6368
Kristiyan Petrov <kris4o1993@gmail.com>
2014-07-21 13:09:35 +0300

    added more tests

---
+++ Labyrinth5.Tests/MazeTests/BacktrackerGeneratorTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Labyrinth5.Common.MazeComponents.Generators;
+using Labyrinth5.Common.MazeComponents.Cells;
+using System.Collections.Generic;
+
+namespace Labyrinth5.Tests.MazeTests
+{
+    [TestClass]
+    public class BacktrackerGeneratorTests
+    {
+        [TestMethod]
+        public void GenerateReturnsBacktrackerCellArray()
+        {
+            BacktrackerMazeGenerator generator = new BacktrackerMazeGenerator();
+            var array = generator.Generate(10, 10);
+            Assert.IsInstanceOfType(array, typeof(Array));
+        }
+    }
+}
